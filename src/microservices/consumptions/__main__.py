5087447808f848efa9534b089ef01420646b98bb
ladypostit <diana.pasquali@alumni.uniroma2.eu>
2022-11-12 17:11:58 +0100

    (ConsumptionsService) Now is giving SummaryService the attention needed

--- src/microservices/consumptions/__main__.py
+++ src/microservices/consumptions/__main__.py
@@ -1,10 +1,22 @@
 import server
 import persistence
+# !/usr/bin/python
+import _thread
+
 
 # python -m grpc_tools.protoc -I ../../proto --python_out=. --pyi_out=. --grpc_python_out=. ../../proto/*.proto --experimental_allow_proto3_optional
 
-print("Hello<3")
-cass = persistence.Cassandra()
-# cass.select_entries_for_product("SELECT * FROM dataset WHERE product_name='Pane';")
-cass.select_entries_for_product('Pane')
-server.serve()
+def wait_for_dataset_entries():
+    print("Hello<3")
+    cass = persistence.Cassandra()
+    cass.select_entries_for_product('Pane')
+
+
+# Create two threads as follows
+try:
+    _thread.start_new_thread(wait_for_dataset_entries, ())
+    _thread.start_new_thread(server.serve(), ())
+except OSError:
+    print("Error: unable to start thread")
+while 1:
+    pass
