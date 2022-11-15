8e9c92708d00e9e15f2599867e772702260a8e89
ladypostit <diana.pasquali@alumni.uniroma2.eu>
2022-11-15 11:44:04 +0100

    (ConsumptionsService) Fixed regression with a different model for each product + now training only for last week and last products added.

--- src/microservices/consumptions/consumptions.py
+++ src/microservices/consumptions/consumptions.py
@@ -15,8 +15,8 @@ import persistence
 # label set prende tutte le righe e le colonne (di queste tutte tranne l'ultima)
 
 def time_series_train_test_split(dataset: ndarray, train_rows: int):
-    # train rows cambiano a ogni iterazione, rappresentano la settimana.
-    # 1) train rows = settimana 1, testing settimane 2-20
+    # Train rows cambiano a ogni iterazione, rappresentano la settimana.
+    # -> train rows = settimana 1, testing settimane 2-20
     data_train = dataset[:train_rows]
     data_test = dataset[train_rows:train_rows + 1]  # dalla seconda in poi
     print("data_train: ", data_train)
@@ -50,12 +50,17 @@ class ConsumptionEstimator:
     def __init__(self, cassandra: persistence.Cassandra):
         self.cassandra_conn = cassandra
         self.total_dataset = None
-        self.product_list = ["Pane", "Mortadella", "Pasta"]
+        self.total_product_list = ["Pane", "Mortadella", "Pasta"]
+        self.last_added_products = []
         self.week_indexes = {"Pane": 20, "Mortadella": 20,
                              "Pasta": 20}  # dizionario degli indici di split per ogni prodotto
         # SGD si adatta maggiormente in base agli ultimi dati analizzati. Piu' preciso rispetto al regressore base.
         # Se ci sono variazioni elevate, non funziona bene. Invece il Regressore lineare si basa su tutti i dati.
-        self.model = SGDRegressor(fit_intercept=True, shuffle=False, warm_start=True, learning_rate='adaptive')
+        self.models = {
+            "Pane": SGDRegressor(fit_intercept=True, shuffle=False, warm_start=True, learning_rate='adaptive'),
+            "Mortadella": SGDRegressor(fit_intercept=True, shuffle=False, warm_start=True, learning_rate='adaptive'),
+            "Pasta": SGDRegressor(fit_intercept=True, shuffle=False, warm_start=True, learning_rate='adaptive')
+        }
         self.product_datasets_dict = {}  # dizionario dei dataset per ogni prodotto
         self.product_X_train_dict = {}  # dizionario delle features di training per ogni prodotto
         self.product_X_test_dict = {}  # dizionario delle features di testing per ogni prodotto
@@ -65,21 +70,19 @@ class ConsumptionEstimator:
         print("Init ML pipeline")
         self.__init_train_dataset()
 
-    def pipeline_training(self):
-        # Update the total dataset
-        self.total_dataset = self.__update_dataset()  # FIXME è necessario, ma bisogna ordinare il select da cassandra
-        print("week indexes: ", self.week_indexes)
+    def new_training(self):
+        self.last_added_products = []
 
-        for prod_name in self.product_list:
-            print("Init dataset ", prod_name)
+    def initial_pipeline_training(self):
+        for prod_name in self.total_product_list:
+            print("Initial pipeline training for ", prod_name)
             self.__init_per_product_datasets(prod_name)
-            print(self.product_datasets_dict[prod_name])
-            print("Init preprocessing ", prod_name)
+            print("Initial pipeline preprocessing ", prod_name)
             self.__preprocessing(prod_name)
-            print(self.product_datasets_dict[prod_name])
             current_week = self.week_indexes[prod_name]  # get the week index for the product selected
             if current_week == 1:
-                # if there is only one observation, skip splitting and scaling
+                # if there is only one observation, skip splitting and scaling and set value 0 to last y_pred
+                self.product_last_y_pred[prod_name] = 0
                 continue
             for week_index in range(1, current_week):
                 print("Splitting dataset ", prod_name)
@@ -89,13 +92,35 @@ class ConsumptionEstimator:
                 print("Training dataset ", prod_name)
                 self.__train_model(prod_name)
 
+    def online_pipeline_training(self):
+        # Update the total dataset
+        self.total_dataset = self.__update_dataset()
+        print("week indexes: ", self.week_indexes)
+
+        for prod_name in self.last_added_products:  # fixme magari training solo per quelli aggiunti ora
+            print("Online pipeline training for ", prod_name)
+            self.__init_per_product_datasets(prod_name)
+            print("Online pipeline preprocessing for ", prod_name)
+            self.__preprocessing(prod_name)
+            current_week = self.week_indexes[prod_name]  # get the week index for the product selected
+            if current_week == 1:
+                # if there is only one observation, skip splitting and scaling and set value 0 to last y_pred
+                self.product_last_y_pred[prod_name] = 0
+                continue
+            print("Splitting dataset ", prod_name)
+            self.__split_training_testing(prod_name, current_week - 1)
+            print("Scaling dataset ", prod_name)  # serve a normalizzare le quantità alle percentuali
+            self.__scaling(prod_name)
+            print("Training dataset ", prod_name)
+            self.__train_model(prod_name)
+
     def predict_consumptions(self):
         """
         Get consumption predictions
         :return: dictionary of consumption prediction for each product present
         """
         res = {}
-        for prod in self.product_list:
+        for prod in self.total_product_list:
             res[prod] = {}
             res[prod]["week"] = self.week_indexes[prod]
             res[prod]["predicted"] = self.product_last_y_pred[prod]
@@ -107,16 +132,24 @@ class ConsumptionEstimator:
         :param product_name: the product to add
         :return: void
         """
-        if self.product_list.__contains__(product_name):
+        # Add product to last added products list
+        self.last_added_products.append(product_name)
+        # Add product to total products list
+        if self.total_product_list.__contains__(product_name):
             return
         else:
-            self.product_list.append(product_name)
+            self.total_product_list.append(product_name)
+            self.models[product_name] = SGDRegressor(fit_intercept=True, shuffle=False, warm_start=True,
+                                                     learning_rate='adaptive')
             self.week_indexes[product_name] = 0
-        print("products list: ", self.product_list)
+        print("products list: ", self.total_product_list)
 
-    def increment_week(self, product_name: str):
+    def increment_week(self, product_name: str, week_num: int):  # TODO mandargli il valore della vera settimana
         old_idx = self.week_indexes[product_name]
         self.week_indexes[product_name] = old_idx + 1
+        print("Actual Week: ", week_num)
+        print("Last registered Week: ", old_idx)
+        print("Week considered: ", self.week_indexes[product_name])
 
     def __init_train_dataset(self):
         dataset = pd.read_csv('consumi-storage.csv', skipinitialspace=True)
@@ -124,7 +157,7 @@ class ConsumptionEstimator:
         self.total_dataset = np.array(dataset.iloc[:, :].values)
 
         # train the initial dataset
-        self.pipeline_training()
+        self.initial_pipeline_training()
 
     def __init_per_product_datasets(self, prod_name):
         prod_data = get_product_dataset(self.total_dataset, prod_name)
@@ -177,10 +210,10 @@ class ConsumptionEstimator:
         y_train = self.product_y_train_dict[prod_name]
         X_test = self.product_X_test_dict[prod_name]
         y_test = self.product_y_test_dict[prod_name]
-        self.model.partial_fit(X_train, y_train)  # a differenza di fit, esegue un addestramento parziale di tipo
+        self.models[prod_name].partial_fit(X_train, y_train)  # a differenza di fit, esegue un addestramento parziale di tipo
         # walk forward
         # (training 1, testing 2 .. training 1-2, testing 3 ..)
-        y_pred = self.model.predict(X_test)
+        y_pred = self.models[prod_name].predict(X_test)
         self.product_last_y_pred[prod_name] = y_pred[0]
         # if week == last_week-1:
         print("y_pred:", y_pred)
