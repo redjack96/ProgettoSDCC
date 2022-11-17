c835fa4c02c2a7eb11b46f39bf4a3fbf5d2c6031
ladypostit <diana.pasquali@alumni.uniroma2.eu>
2022-11-18 00:47:03 +0100

    (Frontend) Implemented Notifications frontend (to be fixed)

--- src/frontend/src/Services/Notifications.tsx
+++ src/frontend/src/Services/Notifications.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Container} from "react-bootstrap";
+import React, {useState} from "react";
+import {Alert, Button, Col, Container, Row} from "react-bootstrap";
 import {
     MDBBtn,
     MDBCard,
@@ -18,9 +18,62 @@ import {
     MDBTypography,
 } from "mdb-react-ui-kit";
 import Navbar from "../Navigation/Utils/Navbar";
+import {ResponsiveContainer} from "recharts";
+import {API_GATEWAY_ADDRESS} from "./Home";
 
 export function Notifications() {
+    const [notifications, setNotifications] = useState({
+        notification: []
+    });
+    const [loading, setLoading] = React.useState(false)
+    const [voidMessage, setVoidMessage] = React.useState("Nothing to notify.");
+
+    // called on page load, loads the entire list from shopping_list microservice
+    React.useEffect(() => {
+        if (!loading) {
+            console.log("reloading list from server");
+            setLoading(true);
+            fetch(API_GATEWAY_ADDRESS + '/getNotifications')
+                .then(r => {
+                    let x = r.json();
+                    console.log(x);
+                    return x;
+                })
+                .then(itemsOrError => {
+                    try {
+                        setNotifications(itemsOrError)
+                        setVoidMessage("Nothing to notify.")
+                    } catch {
+                        console.log("Error: shopping_list service is down")
+                        setNotifications({
+                            notification: []
+                        })
+                        setVoidMessage(itemsOrError.msg)
+                    }
+                })
+                .catch(e => console.log("Errore: " + e))
+            console.log(notifications);
+        }
+    }, [notifications]);
+
     return(
-        <p>Notifications</p>
+        <React.Fragment>
+                {notifications.notification.length === 0 && (
+                    <p className="text-center">{voidMessage}</p>
+                )}
+                {notifications.notification.length != 0 && (
+                    <React.Fragment>
+                        <Alert key='warning1' variant='warning'>
+                            {notifications.notification[0]}
+                        </Alert>
+                        <Alert key='warning2' variant='warning'>
+                            <Col>
+                                {notifications.notification[1]}
+                                <Button className="ml-3" variant="outline-warning">Warning</Button>{' '}
+                            </Col>
+                        </Alert>
+                    </React.Fragment>
+                )}
+        </React.Fragment>
     )
 }
