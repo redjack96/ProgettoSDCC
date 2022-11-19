b2301ede53b50604b885dfebfa4304622b2b15d0
giacomo <giacomo.redjack@gmail.com>
2022-11-19 20:12:41 +0100

    Fixed menu selection. Added Terraform

--- src/frontend/src/Widgets/AlertWidgets.tsx
+++ src/frontend/src/Widgets/AlertWidgets.tsx
@@ -2,18 +2,20 @@ import Modal from "react-bootstrap/Modal";
 import {Button} from "react-bootstrap";
 import React from "react";
 
-export function ModalAlert({showAlert, centralPart, setShowAlert}){
-    return (<Modal show={showAlert}>
-        <Modal.Header closeButton>
-            <Modal.Title>Added the following products to list</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{centralPart}</Modal.Body>
-        <Modal.Footer>
-            <Button variant="primary" onClick={() => setShowAlert(false)}>
-                Close
-            </Button>
-        </Modal.Footer>
-    </Modal>);
+export function ModalAlert({showAlert, centralPart, setShowAlert}) {
+    return (
+        <Modal show={showAlert}>
+            <Modal.Header closeButton>
+                <Modal.Title>Added the following products to list</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{centralPart}</Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={() => setShowAlert(false)}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
 export function SimpleModalAlert({showAlert, message, setShowAlert}) {
---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+# terraform init (solo la prima volta)
+# terraform plan
+# terraform apply
+# terraform destroy
+
+# Necessario per far funzionare aws
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.34.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["./credentials"] /*Copy-paste the file from the lab*/
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0b0dcb5067f052a63"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
+}
