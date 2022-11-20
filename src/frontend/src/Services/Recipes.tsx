996e397e9ad54a433a6a144333ab883aca71d95f
ladypostit <diana.pasquali@alumni.uniroma2.eu>
2022-11-20 17:45:55 +0100

    (Frontend) Fixed service unavailability in Recipes Service.

--- src/frontend/src/Services/Recipes.tsx
+++ src/frontend/src/Services/Recipes.tsx
@@ -14,8 +14,14 @@ import {API_GATEWAY_ADDRESS} from "./Home";
 export function Recipes() {
     const [loading, setLoading] = React.useState(false)
     const [voidMessage, setVoidMessage] = React.useState("No recipes available for your pantry!");
+    const [serviceAvailable, setServiceAvailable] = React.useState(true);
     const [items, setItems] = React.useState({
-        recipes: []
+        recipes: [{
+            img: "",
+            title: "",
+            missed_ingredients: [],
+            url: ""
+        }]
     });
 
     // called on page load, loads the entire list from shopping_list microservice
@@ -30,14 +36,18 @@ export function Recipes() {
                     return x;
                 })
                 .then(itemsOrError => {
-                    try {
-                        setItems(itemsOrError)
-                        setVoidMessage("No recipes available for your pantry!")
-                    } catch {
-                        console.log("Error: shopping_list service is down")
+                    if (itemsOrError.hasOwnProperty('recipes')) {
+                        // service up
+                        setItems(itemsOrError);
+                        setServiceAvailable(true);
+                        setVoidMessage("No recipes available for your pantry!");
+                    } else if (itemsOrError.hasOwnProperty('msg')) {
+                        // service down
+                        console.log("Error: recipes service is down")
                         setItems({
                             recipes: []
                         })
+                        setServiceAvailable(false);
                         setVoidMessage(itemsOrError.msg)
                     }
                 })
@@ -54,31 +64,47 @@ export function Recipes() {
         }
     }
 
+    const displayMessageOrSpinner = (items) => {
+        if (serviceAvailable) {
+            if (items.recipes.length == 1) {
+                return(<MDBSpinner className="center p-5" color='success'>
+                    <span className='visually-hidden'>Loading...</span>
+                </MDBSpinner>);
+            } else if (items.recipes.length == 0) {
+                return (<p className="text-center">{voidMessage}</p>);
+            }
+        } else if (!serviceAvailable) {
+            return (<p className="text-center">{voidMessage}</p>);
+        }
+    }
+
+    const displayRecipes = (items) => {
+        if (items.recipes.length != 1 || (items.recipes.length == 1 && items.recipes[0].title != "")) {
+            return items.recipes.map(item => (
+                <MDBCol>
+                    <MDBCard>
+                        <MDBCardImage src={item.img} position='top' alt='...'/>
+                        <MDBCardBody>
+                            <MDBCardTitle>{item.title}</MDBCardTitle>
+                            <MDBCardText>
+                                Missing Ingredients:<br/>
+                                {item.missed_ingredients.slice(0, 10).map((ingredient, index, row) => showListIngredients(ingredient, index, row))}
+                            </MDBCardText>
+                            <MDBBtn href={item.url} color="success">Prepare...</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            ));
+        }
+    }
+
     return(
         <Container>
             <Navbar/>
             <PageHeader pageName="Recipes"/>
             <Row xs={1} md={4} className="g-4 fixed scrollable">
-                {items.recipes.length === 0 && (
-                    <MDBSpinner className="center p-5" color='success'>
-                        <span className='visually-hidden'>Loading...</span>
-                    </MDBSpinner>
-                )}
-                {items.recipes.map(item => (
-                    <MDBCol>
-                        <MDBCard>
-                            <MDBCardImage src={item.img} position='top' alt='...' />
-                            <MDBCardBody>
-                                <MDBCardTitle>{item.title}</MDBCardTitle>
-                                <MDBCardText>
-                                    Missing Ingredients:<br/>
-                                    {item.missed_ingredients.slice(0,10).map((ingredient, index, row) => showListIngredients(ingredient, index, row))}
-                                </MDBCardText>
-                                <MDBBtn href={item.url} color="success">Prepare...</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </MDBCol>
-                ))}
+                {displayMessageOrSpinner(items)}
+                {displayRecipes(items)}
             </Row>
         </Container>
     )
